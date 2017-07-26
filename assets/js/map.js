274f6fe942066c862309e678abee2f201a702bad
temsan89 <temsan89@gmail.com>
2017-07-26 07:04:22 +0300

    gmap changes

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,8 +1,22 @@
-(function myMap() {
-    var mapCanvas = document.getElementById("map");
-    var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.2),
-        zoom: 10
-    };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-}())
+(function ($) {
+    var map,
+    pointPin = {lat: 50.4320456, lng: 30.5167456};
+   
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 50.432, lng: 30.516},
+            zoom: 15,
+            disableDefaultUI: false,
+            scrollwheel: false
+        });
+        var image = {
+            url: './img/pin.png'
+        };
+//        var marker = new google.maps.Marker({
+//            position: pointPin,
+//            map: map,
+//            icon: image
+//        });
+    }
+        initMap();
+})(jQuery);
