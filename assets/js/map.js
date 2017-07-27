1612e8a2c113a412bb3a616db4cc6575bc14163c
temsan89 <temsan89@gmail.com>
2017-07-27 17:41:52 +0300

    contscts html/ news.html

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,22 +1,21 @@
-(function ($) {
-    var map,
-    pointPin = {lat: 50.4320456, lng: 30.5167456};
-   
+$(function () {
+
     function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 50.432, lng: 30.516},
-            zoom: 15,
-            disableDefaultUI: false,
-            scrollwheel: false
-        });
-        var image = {
-            url: './img/pin.png'
-        };
-//        var marker = new google.maps.Marker({
-//            position: pointPin,
-//            map: map,
-//            icon: image
-//        });
+
+        var location = new google.maps.LatLng(50.0875726, 14.4189987);
+
+        var mapCanvas = document.getElementById('map');
+        var mapCanvas2 = document.getElementById('map2');
+        var mapOptions = {
+            center: location,
+            zoom: 16,
+            panControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+        var map2 = new google.maps.Map(mapCanvas2, mapOptions);
+
     }
-        initMap();
-})(jQuery);
+
+    google.maps.event.addDomListener(window, 'load', initMap);
+});
