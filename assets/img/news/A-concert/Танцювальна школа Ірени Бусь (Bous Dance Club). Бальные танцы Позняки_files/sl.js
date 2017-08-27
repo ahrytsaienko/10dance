7fc84beb8560e23e0dd88ce88e1eb02f911e6610
Tetiana Hrytsaienko <kortatiana89@gmail.com>
2017-08-27 20:52:00 +0300

    t: fixed bags

---
+++ assets/img/news/A-concert/Танцювальна школа Ірени Бусь (Bous Dance Club). Бальные танцы Позняки_files/sl.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $('.sl').slick({
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 2500,
+        dots: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        responsive: [
+            {
+                breakpoint: 482,
+                settings: 'slick'
+    },
+            {
+                breakpoint: 481,
+                settings: 'unslick'
+      },
+
+      ]
+    });
+});
+
+// SLICK CONTACTS
+
+$(document).ready(function () {
+    $('.slick_contacts').slick({
+        autoplay: true,
+        dots: true
+    });
+});
+
+
+
